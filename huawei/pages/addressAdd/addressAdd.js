500d57221a1a54a073a6e1102912c60a404e3d8c
yangshufei <961320108@qq.com>
2018-06-09 23:27:29 +0800

    '修改了添加地址项'

--- huawei/pages/addressAdd/addressAdd.js
+++ huawei/pages/addressAdd/addressAdd.js
@@ -1,66 +1,44 @@
-// pages/addressAdd/addressAdd.js
+var index = 0;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-  
+  data:{
+    addreValue:0,
+    addreRange:['　　　　　　　　　　','北京','上海','南昌','四川','武汉','广州']
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-  
+  addrePickerBindchange:function(e){
+    this.setData({
+      addreValue:e.detail.value
+    })
   },
+  formSubmit: function(e) {
+    var warn ="";
+    var that = this;
+    var flag = false;
+    if(e.detail.value.name==""){
+      warn = "请填写您的姓名！";
+    }else if(e.detail.value.tel==""){
+      warn = "请填写您的手机号！";
+    }else if(!(/^1(3|4|5|7|8)\d{9}$/.test(e.detail.value.tel))){
+      warn = "手机号格式不正确";
+    }else if(e.detail.value.addre=='0'){
+      warn = "请选择您的所在区域";
+    }else if(e.detail.value.door==""){
+      warn = "请输入您的具体地址";
+    }else{
+      flag=true;
+        wx.redirectTo({
+        url: '../addressList/addressList?tel='+e.detail.value.tel+"&addre="+that.data.addreRange[e.detail.value.addre]+"&door="+e.detail.value.door+"&name="+e.detail.value.name+"&flag="+flag+"&addrevalue="+e.detail.value.addre
+        //？后面跟的是需要传递到下一个页面的参数
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-  
+      }); 
+        //  console.log("传过去的地址下标是多少？"+e.detail.value.addre)
+    }
+    if(flag==false){
+      wx.showModal({
+      title: '提示',
+      content:warn
+    })
+    }
+    
   },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
   
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-  
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-  
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-  
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-  
-  }
-})
+  })
