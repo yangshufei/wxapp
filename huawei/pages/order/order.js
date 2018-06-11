96ae5e0f1116dfb458f160fbec4ff1d00e3d2849
yangshufei <961320108@qq.com>
2018-06-11 16:47:25 +0800

    修改了订单项

--- huawei/pages/order/order.js
+++ huawei/pages/order/order.js
@@ -1,12 +1,63 @@
-// pages/order/order.js
+
+var flag = false;
+var title,price,num,totalPrice;
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-    address:'',
-    hasAddress: false,
+  
+  data:{
+    name:"",
+    tel:"",
+    addre:"",
+    display1:"flex",
+    display2:"none",
   },
-
+  
+  toAddAddre:function(){
+    wx.redirectTo({
+       url: '../addressList/addressList'
+    });
+  },
+ 
+  onLoad: function(options) { 
+      title = options.title;
+      price = options.price;
+      num = options.num;
+      totalPrice = options.totalPrice;
+      flag=options.flag;
+      // console.log(title);
+      this.setData({
+        title,
+        num,
+        price,
+        totalPrice
+      })
+    if(!flag){
+      this.setData({
+          display1:"flex",
+          display2:"none",
+        })
+      
+    }else{
+       this.setData({
+          display1:"none",
+          display2:"flex",
+          name:options.name,
+          tel:options.tel,
+          addre:options.addre
+       })
+    }
+  },
+  onShow(){
+    wx.getStorage({
+      key: 'detail',
+      success: (res)=>{
+        // console.log(res.data);
+        this.setData({
+          title:res.data.name,
+          price:res.data.price,
+          totalPrice:res.data.totalPrice,
+          num:res.data.num
+        })
+      },
+    })
+  },
 })
